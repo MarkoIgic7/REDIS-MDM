36421fce446aa037ed93ef26e8f11afb971e3252
milicavasiljevic00 <121403464+milicavasiljevic00@users.noreply.github.com>
2022-12-30 09:24:19 +0100

    cors

--- MyNews/Program.cs
+++ MyNews/Program.cs
@@ -7,7 +7,38 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => {
+                options.AddPolicy("CORS", builder => {
+                    builder.WithOrigins(new string []
+                    {
+                        "https://localhost:5001",
+                        "http://localhost:5001",
+                        "https://127.0.0.1:5001",
+                        "http://127.0.0.1:5001",
+                        "https://localhost:5500",
+                        "http://localhost:5500",
+                        "https://127.0.0.1:5500",
+                        "http://127.0.0.1:5500",
+                        "https://localhost:3000",
+                        "http://localhost:3000",
+                        "https://127.0.0.1:3000",
+                        "http://127.0.0.1:3000",
+                        "https://localhost:7220",
+                        "http://localhost:7220",
+                        "https://127.0.0.1:7220",
+                        "http://127.0.0.1:7220"
+
+ 
+
+                    })
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,6 +50,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("CORS");
 
 app.MapControllers();
 
